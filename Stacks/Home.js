f95e3aade45d62009b8a67602bfff024c2bb659b
Lovisa Eriksson <lovisa@ahna.se>
2022-05-11 09:25:41 +0200

    clean canvas

--- Stacks/Home.js
+++ Stacks/Home.js
@@ -2,29 +2,12 @@ import {View, Text, Button, SectionList, StyleSheet} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function Home({navigation}) {
-    // const tasks = [
-    //   {
-    //     title: 'Tasks to do:',
-    //     data: [{task: 'Städa', deadline:'2 pm', done: false}]
-    //   },
-    //   {
-    //     title: 'Done with:',
-    //     data: [{task: 'Äta vattenmelon', deadline: '4 pm'}]
-    //   }
-    // ]
-    // const GridList = ({data}) => {
-  
-    // }
+   
+    
   
     return (
       <View style={styles.container}>
         <Text style={styles.title}>THINGS TO DO:</Text>
-        {/* <SectionList 
-          sections={tasks}
-          keyExtractor={(item, index) => item + index}
-          renderItem={({item}) => <Text style={styles.listItem}>- {item}</Text>}
-          renderSectionHeader={({section: {title} }) => <Text style={styles.subTitle}>{title}</Text>}
-        /> */}
         <StatusBar style="auto" />
         <Button
         title="Add a task"
